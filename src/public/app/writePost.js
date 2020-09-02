cea61dba03b4161dbf5bf81340cfcc0b93ddf45f
dhruvshaarmaa <61281854+dhruvshaarmaa@users.noreply.github.com>
2020-09-02 13:52:58 +0530

    Update writePost.js

--- src/public/app/writePost.js
+++ src/public/app/writePost.js
@@ -1,5 +1,6 @@
 function writePost(userId, title, body){
     $.post("/api/posts",{userId, title, body},(post)=>{
         console.log(post);
+        $("#content").load("allPosts.html");
     });
 }
